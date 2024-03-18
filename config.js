d695bd48f188aed72f236d448faddf3d13bf142a
khanh <vankhanhhai2003@gmail.com>
2024-03-18 22:26:02 +0700

    upload

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à! \n Honey Trinh Thị Hòa',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Babe à! \n Honey Trịnh Thị Hòa',
+    introDesc: `Luật pháp nào cấm nam yêu nữ.
+    Công lý nào cấm nữ yêu nam.
+    Anh sẽ mở phiên tòa xét xử.
+    Xét vì tội : Em cướp trái tim anh.`,
+    btnIntro: 'Nữa nè hihi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu Bắc ^^',
+    btnNo: 'No, Baby:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Cuối tuần này anh đón bé đi chơi nhé!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/ron.bac.14811' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
